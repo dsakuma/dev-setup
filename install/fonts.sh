d3589b2d6a9f28e9034970dfd8d925646eeab156
Daniel Sakuma <dsakuma@gmail.com>
2024-06-16 19:36:51 -0300

    Add fonts

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,2 +1,10 @@
-sudo apt install -y \
-    fonts-jetbrains-mono
+mkdir -p ~/.fonts/Meslo
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip -P /tmp
+unzip /tmp/Meslo.zip -d ~/.fonts/Meslo
+
+
+mkdir -p ~/.fonts/JetBrainsMono
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip -P /tmp
+unzip /tmp/JetBrainsMono.zip -d ~/.fonts/JetBrainsMono
+
+fc-cache -fv

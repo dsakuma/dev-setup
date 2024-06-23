375e4d70cef50b2c0821bafcdba1cb9dddf4f300
Daniel Sakuma <dsakuma@gmail.com>
2024-06-23 10:25:09 -0300

    Disable SC1090

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 directories="pre-install install setup dotfiles post-dotfiles post-install"
 for directory in $directories; do
 	echo Running "$directory" scripts...
+	# shellcheck disable=SC1090
 	for script in ./"$directory"/*.sh; do . "$script"; done
 done
 

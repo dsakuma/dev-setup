096c941bf135911395258d3bd860030e2354159f
Daniel Sakuma <dsakuma@gmail.com>
2024-06-23 20:54:01 -0300

    Add chezmoi comment

--- dotfiles/dotfiles.sh
+++ dotfiles/dotfiles.sh
@@ -3,3 +3,9 @@
 homesick clone git@github.com:dsakuma/dotfiles.git
 homesick link dotfiles
 echo "Restart session run or run \"exec zsh -l\""
+
+# Use chezmoi
+# cd /tmp || exit
+# wget https://github.com/twpayne/chezmoi/releases/download/v2.49.1/chezmoi_2.49.1_linux_amd64.deb
+# apt install -y ./chezmoi_2.49.1_linux_amd64.deb
+# cd - || exit

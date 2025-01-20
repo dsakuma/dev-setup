390f000306d4f6e041f1537577e24cd20d3174bb
Daniel Sakuma <dsakuma@gmail.com>
2025-01-19 23:13:16 -0300

    Use brew for chezmoi

--- 02-install-terminal/app-chezmoi.sh
+++ 02-install-terminal/app-chezmoi.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# Use chezmoi
-# cd /tmp || exit
-# wget https://github.com/twpayne/chezmoi/releases/download/v2.49.1/chezmoi_2.49.1_linux_amd64.deb
-# apt install -y ./chezmoi_2.49.1_linux_amd64.deb
-# cd - || exit
+brew install chezmoi

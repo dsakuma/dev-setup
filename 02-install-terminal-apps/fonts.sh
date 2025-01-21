f27e1a0e75e45abb21813307e1a9b45257e6f39b
Daniel Sakuma <dsakuma@gmail.com>
2025-01-20 23:05:06 -0300

    Fix folder name

--- 02-install-terminal-apps/fonts.sh
+++ 02-install-terminal-apps/fonts.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-mkdir -p ~/.fonts/Meslo
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip -P /tmp
-unzip /tmp/Meslo.zip -d ~/.fonts/Meslo
+if [ ! -d ~/.fonts/Meslo ]; then
+  mkdir -p ~/.fonts/Meslo
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Meslo.zip -P /tmp
+  unzip /tmp/Meslo.zip -d ~/.fonts/Meslo
+fi
 
-# mkdir -p ~/.fonts/JetBrainsMono
-# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip -P /tmp
-# unzip /tmp/JetBrainsMono.zip -d ~/.fonts/JetBrainsMono
+
+# if [ ! -d ~/.fonts/JetBrainsMono ]; then
+#   mkdir -p ~/.fonts/JetBrainsMono
+#   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip -P /tmp
+#   unzip /tmp/JetBrainsMono.zip -d ~/.fonts/JetBrainsMono
+# fi
 
 fc-cache -fv

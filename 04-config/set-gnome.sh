adcc17aea44b62739d04f39a7b86ab93ce679656
Daniel Sakuma <dsakuma@gmail.com>
2025-01-21 23:05:17 -0300

    Set nightlight

--- 04-config/set-gnome.sh
+++ 04-config/set-gnome.sh
@@ -17,5 +17,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 # Hide mounted volumes and devices
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 
+# Set night light
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4700
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic true
+
 # Set dark mode
 dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+

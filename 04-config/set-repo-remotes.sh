d22f8e48a6912f2682cb89a35bb2619b860e3401
Daniel Sakuma <dsakuma@gmail.com>
2025-01-26 10:14:03 -0300

    Set remote repo

---
+++ 04-config/set-repo-remotes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+git -C ~/.local/share/dev-setup remote set-url origin "git@github.com:dsakuma/dev-setup.git"
+git -C ~/.homesick/repos/dotfiles remote set-url origin "git@github.com:dsakuma/dotfiles.git"

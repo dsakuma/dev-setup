9e564364e132e0ec34c0776918c51473d711982d
Daniel Sakuma <dsakuma@gmail.com>
2025-01-25 22:04:07 -0300

    Exit when a script fails

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-set -eu
-
 directories=("01-pre-install 02-install-terminal-apps 03-install-desktop-apps 04-config 05-post-install")
 
 for directory in ${directories[@]}; do
 	echo Running "$directory" scripts...
 	# shellcheck disable=SC1090
 	for script in ./"$directory"/*.sh; do 
-	  source "$script"; 
+	  source "$script"
+	  if [ $? -ne 0 ]; then
+		echo "Error: $script failed to execute."  
+	    return 1
+	  fi
 	done
 done
 

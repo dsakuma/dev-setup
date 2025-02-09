6b5c5d3574531793ccb61fbfbb82cc58c8b4ca3c
Daniel Sakuma <dsakuma@gmail.com>
2025-02-08 22:05:48 -0300

    Add sudo

--- 01-pre-install/pre-requisites.sh
+++ 01-pre-install/pre-requisites.sh
@@ -6,7 +6,7 @@ sudo apt install -y build-essential curl unzip
 
 # Flatpak and Flathub
 sudo apt install -y flatpak gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Homebrew
 CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

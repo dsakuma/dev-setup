1d85458a30d78d5b2f08ea1e64fe208dded4ceeb
Daniel Sakuma <dsakuma@gmail.com>
2025-10-22 23:39:17 -0300

    Update link-dotfiles.sh

--- 04-config/link-dotfiles.sh
+++ 04-config/link-dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-homesick clone https://github.com/dsakuma/dotfiles.git
+homesick clone https://github.com/dsakuma/dotfiles-dev-setup.git
 homesick link dotfiles
 
-git -C ~/.homesick/repos/dotfiles remote set-url origin "git@github.com:dsakuma/dotfiles.git"
+git -C ~/.homesick/repos/dotfiles remote set-url origin "git@github.com:dsakuma/dotfiles-dev-setup.git"
